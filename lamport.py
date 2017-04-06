bb7c1b0ed0383de0813affc0f4bf39dc775f2440
Nikita <robots+github@begishev.me>
2017-04-06 06:38:03 +0300

    Extract LamportBase class for unit testing

--- lamport.py
+++ lamport.py
@@ -6,118 +6,178 @@ import sys
 import signal
 import heapq
 import fcntl
-import random
 
 from RPC.rpc import RPC, EV_REMOTE
 from RPC import commands
 
-BUF_SZ = 1024
-EV_STDIN = 1
-EV_TIMER = 2
 
-def stdin_handler(ev_id, fd):
-    return ev_id, fd
+class LamportBase:
+    def __init__(self, stress_mode=False):
+        self._clock = 0
+        self._requests = set()
+        self._queue = []
+        self.EV_TIMER = 2
+        self.EV_STDIN = 1
+        self.EV_REMOTE = 0
+        self.stress_mode = stress_mode
 
-def stress_mode_handler(ev_id, fd):
-    os.read(fd, 1)
-    return ev_id, None
+        pipe_r, pipe_w = os.pipe()
+        flags = fcntl.fcntl(pipe_w, fcntl.F_GETFL, 0)
+        flags = flags | os.O_NONBLOCK
+        fcntl.fcntl(pipe_w, fcntl.F_SETFL, flags)
 
-def parse_addr(addr_str):
-    ip, port = addr_str.split(':')
-    return (ip, int(port))
+        signal.set_wakeup_fd(pipe_w)
+        signal.signal(signal.SIGALRM, lambda *_: None)
+        signal.setitimer(signal.ITIMER_REAL, 1, 1)
 
-SELF_ADDR = parse_addr(sys.argv[1])
-OTHER_ADDRS = list(map(parse_addr, sys.argv[2:]))
+        self.reg_for_poll(pipe_r, self._timer_handler, ev_id=self.EV_TIMER)
 
-rpc = RPC(SELF_ADDR, OTHER_ADDRS)
-self_id = rpc.host_by_addr[SELF_ADDR]
+    @staticmethod
+    def parse_addr(addr_str):
+        ip, port = addr_str.split(':')
+        return ip, int(port)
 
-clock = 0
-requests = set()
-queue = []
+    def enable_console(self):
+        self.reg_for_poll(sys.stdin.fileno(), LamportBase._stdin_handler, self.EV_STDIN)
 
-rpc.reg_for_poll(sys.stdin.fileno(), stdin_handler, ev_id=EV_STDIN)
+    @staticmethod
+    def _stdin_handler(ev_id, fd):
+        return ev_id, fd
 
-pipe_r, pipe_w = os.pipe()
-flags = fcntl.fcntl(pipe_w, fcntl.F_GETFL, 0)
-flags = flags | os.O_NONBLOCK
-flags = fcntl.fcntl(pipe_w, fcntl.F_SETFL, flags)
+    @staticmethod
+    def _timer_handler(ev_id, fd):
+        os.read(fd, 1)
+        return ev_id, 0
 
-rpc.reg_for_poll(pipe_r, stress_mode_handler, ev_id=EV_TIMER)
+    def host_id(self):
+        raise NotImplementedError
 
-stress_mode = True
+    def hosts(self):
+        raise NotImplementedError
 
-for host_id in rpc.hosts_ids:
-    requests.add(host_id)
+    def host_index(self, host_id):
+        raise NotImplementedError
 
-host_index = {rpc.host_by_addr[addr]: idx for idx, addr in enumerate(sorted(rpc.addrs))}
+    # Interface for events embedding
+    def reg_for_poll(self, fd, handler, ev_id):
+        raise NotImplementedError
 
-print("RPC Initialized.")
+    def send(self, host_id, cmd, *args):
+        raise NotImplementedError
 
-if stress_mode:
-    clock += 1
-    for host_id in rpc.hosts_ids:
-        rpc.send_to(host_id, commands.REQ, clock)
+    def send_all(self, cmd, *args):
+        raise NotImplementedError
 
-for ev in rpc.events_loop():
-    ev_name, ev_data = ev[0], ev[1:]
+    def acquire(self):
+        raise NotImplementedError
 
-    if ev_name == EV_STDIN:
-        buf = os.read(ev_data[0], BUF_SZ).strip()
+    @staticmethod
+    def _critical_section(pid, clock):
+        raise NotImplementedError
 
-        if buf == 'acquire':
-            clock += 1
-            for host_id in rpc.hosts_ids:
-                requests.add(host_id)
-                rpc.send_to(host_id, commands.REQ, clock)
-        elif buf == 'stress start':
-            stress_mode = True
-        elif buf == 'stress stop':
-            stress_mode = False
-        elif buf == 'stop':
-            rpc.send_to(self_id, commands.STOP)
+    def _transport_loop(self):
+        raise NotImplementedError
 
-    if ev_name == EV_REMOTE:
-        host_id, cmd, args = ev_data[0], ev_data[1], ev_data[2]
-        print(commands.names[cmd], ev_data)
+    def events_loop(self):
+        for ev in self._transport_loop():
+            ev_name, ev_data = ev[0], ev[1:]
 
-        if cmd == commands.REQ:
-            clock_ = int(args[0])
-            clock = max(clock + 1, clock_)
-            heapq.heappush(queue, (clock_, host_index[host_id]))
-            rpc.send_to(host_id, commands.RES)
+            if ev_name != self.EV_REMOTE:
+                yield ev
+                continue
 
-        if cmd == commands.RES:
-            requests.remove(host_id)
+            host_id, cmd, args = ev_data[0], ev_data[1], ev_data[2]
+            print(commands.names[cmd], ev_data)
 
-        if cmd == commands.REL:
-            clock_ = int(args[0])
-            clock = max(clock + 1, clock_)
-            queue = filter(
-                lambda (_, host_idx): host_index[host_id] != host_idx,
-                queue
-            )
-            heapq.heapify(queue)
+            if cmd == commands.REQ:
+                clock = int(args[0])
+                self._clock = max(self._clock + 1, clock)
+                heapq.heappush(self._queue, (clock, self.host_index(host_id)))
+                self.send(host_id, commands.RES)
 
-        if cmd in [commands.RES, commands.REL]:
-            if not requests and queue[0][1] == host_index[self_id]:
-                print(">>>>> CS enter")
-                mutex_file = open("mutex.txt", "w")
-                fcntl.flock(mutex_file, fcntl.LOCK_EX | fcntl.LOCK_NB)
-                mutex_file.write(str(clock))
-                fcntl.flock(mutex_file, fcntl.LOCK_UN)
-                mutex_file.close()
-                print("<<<<< CS exit")
+            elif cmd == commands.RES:
+                self._requests.remove(host_id)
 
-                clock += 1
-                for host_id in rpc.hosts_ids:
-                    rpc.send_to(host_id, commands.REL, clock)
-                    requests.add(host_id)
+            elif cmd == commands.REL:
+                clock = int(args[0])
+                self._clock = max(self._clock + 1, clock)
+                self._queue = filter(
+                    lambda (_, host_idx): self.host_index(host_id) != host_idx,
+                    self._queue
+                )
+                heapq.heapify(self._queue)
 
-                if stress_mode:
-                    clock += 1
-                    for host_id in rpc.hosts_ids:
-                        rpc.send_to(host_id, commands.REQ, clock)
+            if cmd in [commands.RES, commands.REL]:
+                if not self._requests and self._queue[0][1] == self.host_index(self.host_id()):
+                    print(">>>>> CS enter")
+
+                    self._critical_section(os.getpid(), self._clock)
+
+                    print("<<<<< CS exit")
+
+                    self.send_all(commands.REL, self._clock)
+                    for host_id in self.hosts():
+                        self._requests.add(host_id)
+
+                    if self.stress_mode:
+                        self.acquire()
+
+            yield ev_name, host_id
 
 
-print("Stopped.")
+class LamportRPC(LamportBase):
+    def __init__(self, self_addr, other_addrs, stress_mode=False):
+        self.EV_REMOTE = EV_REMOTE
+
+        self._rpc = RPC(self_addr, other_addrs)
+        self._host_id = self._rpc.host_by_addr[self_addr]
+
+        LamportBase.__init__(self, stress_mode)
+
+        for host_id in self._rpc.hosts_ids:
+            self._requests.add(host_id)
+
+        self._host_index = {
+            self._rpc.host_by_addr[addr]: idx
+            for idx, addr in enumerate(sorted(self._rpc.addrs))
+        }
+
+        if stress_mode:
+            self.send_all(commands.REQ, self._clock)
+
+    def host_id(self):
+        return self._host_id
+
+    def hosts(self):
+        return self._rpc.hosts_ids
+
+    def host_index(self, host_id):
+        return self._host_index[host_id]
+
+    def reg_for_poll(self, fd, handler, ev_id):
+        self._rpc.reg_for_poll(fd, handler, ev_id)
+
+    def send(self, host_id, cmd, *args):
+        self._clock += 1
+        self._rpc.send_to(host_id, cmd, *args)
+
+    def send_all(self, cmd, *args):
+        self._clock += 1
+        for host_id in self._rpc.hosts_ids:
+            self._rpc.send_to(host_id, cmd, *args)
+
+    def acquire(self):
+        self.send_all(commands.REQ, self._clock)
+        for host_id in self.hosts():
+            self._requests.add(host_id)
+
+    @staticmethod
+    def _critical_section(pid, clock):
+        mutex_file = open("mutex.txt", "w")
+        fcntl.flock(mutex_file, fcntl.LOCK_EX | fcntl.LOCK_NB)
+        mutex_file.write("{} {}".format(pid, clock))
+        fcntl.flock(mutex_file, fcntl.LOCK_UN)
+        mutex_file.close()
+
+    def _transport_loop(self):
+        return self._rpc.events_loop()
